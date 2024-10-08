ff485b72b1cd6bab3adbd693afc50b4961205a27
Sanches Spoladore <sanches.lvs@gmail.com>
2024-10-08 19:09:51 -0300

    Algumas correções e melhorias para fazer a criação do banco de dados e inserir as informações

--- main.py
+++ main.py
@@ -3,26 +3,41 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, RedirectResponse
 from starlette.requests import Request
+import sqlite3
 import uvicorn
+from sql.produto_sql import SQL_CRIAR_TABELA, SQL_INSERIR
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def criar_tabela():
+    conn = sqlite3.connect('produtos.db')
+    cursor = conn.cursor()
+    cursor.execute(SQL_CRIAR_TABELA)
+    conn.commit()
+    conn.close()
 
 @app.get("/", response_class=HTMLResponse)
 def paginaInicial(request: Request):
-  return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/cadastro", response_class=HTMLResponse)
 async def cadastro(request: Request):
-  return templates.TemplateResponse("cadastro.html", {"request": request})
+    return templates.TemplateResponse("cadastro.html", {"request": request})
 
 @app.post("/post_cadastro")
 def post_cadastro(nome: str = Form(...), descricao: str = Form(...), estoque: int = Form(...), preco: float = Form(...), categoria: str = Form(...)):
-  return RedirectResponse("/", status_code=303)
+    conn = sqlite3.connect('produtos.db')
+    cursor = conn.cursor()
 
+    cursor.execute(SQL_INSERIR, (nome, descricao, estoque, preco, categoria))
+    conn.commit()
+    conn.close()
+
+    return RedirectResponse("/", status_code=303)
 
 if __name__ == "__main__":
-  uvicorn.run("main:app", port=8000, reload=True)
+    criar_tabela()
+    uvicorn.run("main:app", port=8000, reload=True)
